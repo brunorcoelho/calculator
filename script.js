c15b8899d654a17e8d32a537c9c75dafe9afeeb4
Bruno Coelho <bruno090904@gmail.com>
2022-06-23 15:10:27 -0400

    Functional visor and clear button

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const visor = document.getElementById("result");
+const clear = document.getElementById("clear");
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+
+clear.addEventListener("click", clearVisor);
+zero.addEventListener("click", addToVisor);
+one.addEventListener("click", addToVisor);
+two.addEventListener("click", addToVisor);
+three.addEventListener("click", addToVisor);
+four.addEventListener("click", addToVisor);
+five.addEventListener("click", addToVisor);
+six.addEventListener("click", addToVisor);
+seven.addEventListener("click", addToVisor);
+eight.addEventListener("click", addToVisor);
+nine.addEventListener("click", addToVisor);
+
+function addToVisor() {
+  var numberToAdd = document.createTextNode(this.innerHTML);
+  visor.appendChild(numberToAdd);
+}
+
+function clearVisor() {
+  visor.innerHTML = "";
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
